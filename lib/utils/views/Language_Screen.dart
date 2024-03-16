46fcfd22dda3fb762fe53e3b73e372bdede2e26f
Admin <maayush235@gmail.com>
2024-03-17 00:46:01 +0530

    Radhey radhey

--- lib/utils/views/Language_Screen.dart
+++ lib/utils/views/Language_Screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:resume/utils/colors/color.dart';
 
@@ -13,46 +14,83 @@ class _Language_ScreenState extends State<Language_Screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 70,
-              width: 420,
-              decoration: BoxDecoration(
-                  color: const Color(0xffE0E0E0),
-                  borderRadius: BorderRadius.circular(15)
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  children: [
-                    const Text('English',style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w400
-                    ),),
-                    const SizedBox(width: 35,),
-                    const Icon(Icons.keyboard_arrow_down_outlined,size: 26,),
-                    const SizedBox(width: 200,),
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration:  BoxDecoration(
-                          color: maincolor
+              Container(
+
+                height: 70,
+                width: 420,
+                decoration: BoxDecoration(
+                    color:  Color(0xffE0E0E0),
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    children: [
+                       Text('English',style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400
+                      ),),
+                       SizedBox(width: 35,),
+                       Icon(Icons.keyboard_arrow_down_outlined,size: 26,),
+                       SizedBox(width: 200,),
+                      Container(
+                        height: 45,
+                        width: 45,
+                        decoration:  BoxDecoration(
+                            color: maincolor,
+                          borderRadius: BorderRadius.all(Radius.circular(5))
+                        ),
+                        child:  Icon(Icons.add,size: 30,),
                       ),
-                      child: const Icon(Icons.add,size: 30,),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            )
-          ],
+
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: maincolor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Add education',style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),),
+
+                    Icon(Icons.add,size: 30,)
+                  ],
+                  ),
+                ),
+              )
+
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
